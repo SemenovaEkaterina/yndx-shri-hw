b54a9f9c6375d3cd6899a4dd1a4205138f4b85cf
Kate Semenova <katesemenova@Grigorys-MacBook-Air.local>
2019-09-21 21:46:11 +0300

    add mobile version

--- front/static/script.js
+++ front/static/script.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var openedClass = 'dropdown_opened';
-    document.querySelectorAll('.dropdown__arrow').forEach(function (item) {
+    var openedClass = 'Dropdown_opened';
+    document.querySelectorAll('.Dropdown-arrow').forEach(function (item) {
         item.addEventListener('click', function (e) {
             e.target.parentNode.classList.toggle(openedClass);
         })
     });
-    document.querySelectorAll('.dropdown__options').forEach(function (item) {
+    document.querySelectorAll('.Dropdown-options').forEach(function (item) {
         item.addEventListener('click', function (e) {
             e.currentTarget.parentNode.classList.remove(openedClass);
         })
     });
 
-    document.querySelectorAll('.code__text').forEach((block) => {
+    document.querySelectorAll('.Code-text').forEach((block) => {
         hljs.highlightBlock(block);
     });
 });
